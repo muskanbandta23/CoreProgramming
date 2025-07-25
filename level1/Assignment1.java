f75f3ccb024fa007f5a080cb8cc4370d15c1f23b
Muskan Bandta <muskanbandta1@gmail.com>
2025-07-25 14:55:43 +0530

    Added Assignment1.java inside level1 folder

---
+++ level1/Assignment1.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.util.Scanner;
+public class Assignment1 {
+    public static void main(String[] args){
+        // Q-1. add 2 numbers
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Bridgelabz!");
+
+
+//        int num1 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//        int result = num1+num2;
+//        System.out.println("Sum of two number "+ result);
+
+
+        //Q-2 . Celsius to Fahrenheit Conversion
+//        double celsius = sc.nextDouble();
+//        double fahrenheit = (celsius * 9/5) +32;
+//        System.out.println("fahrenheit"+ fahrenheit);
+
+
+        //Q-3 . Area of a Circle
+//        double radius = sc.nextFloat();
+//        double area = 3.14* radius * radius;
+//        System.out.println("Area "+ area);
+
+
+        //Q-4 . Volume of a Cylinder
+//           double radius =sc.nextDouble();
+//           double height = sc.nextDouble();
+//           double volume = 3.14 * radius * radius * height;
+//           System.out.println("Volume "+ volume);
+
+
+        //Q-5 .Calculate simple interest
+//        int princi = sc.nextInt();
+//        int rate = sc.nextInt();
+//        int time = sc.nextInt();
+//        int si = (princi*rate*time)/100;
+//        System.out.println("Simple Intrest "+ si);
+
+
+
+        //Q-7 . Perimeter of rectangle
+//        int len = sc.nextInt();
+//        int width = sc.nextInt();
+//        float perimeter = 2 *(len+width);
+//        System.out.println("Perimeter "+perimeter);
+
+
+        //Q-8 . power calculation
+//        int base = sc.nextInt();
+//        int exponent = sc.nextInt();
+//        double res = Math.pow(base, exponent);
+//        System.out.println("Result "+ res);
+
+
+        //Q - 9 . Calculate avg of three numbers
+//        int num1 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//        int num3 = sc.nextInt();
+//        int avg = (num1+num2+num3)/2;
+//        System.out.println("Average "+ avg);
+
+
+        //Q- 10 . Convert kilometers into miles
+//        float dist = sc.nextFloat();
+//        double miles = dist * 0.621371;
+//        System.out.println("Miles distance "+ miles);
+
+
+
+
+    }
+}
