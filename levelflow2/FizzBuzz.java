bf99be0ee761faf6361e09f82ece436656c56dff
Muskan Bandta <muskanbandta1@gmail.com>
2025-07-27 17:24:49 +0530

    Question 5 and Question 5

---
+++ levelflow2/FizzBuzz.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        int num = scn.nextInt();
+//        if(num > 0){
+//            for(int i = 1; i <= num; i++) {
+//                if(i % 3 == 0 && i % 5 == 0) {
+//                    System.out.println("FizzBuzz");
+//                } else if(i % 3 == 0) {
+//                    System.out.println("Fizz");
+//                } else if(i % 5 == 0) {
+//                    System.out.println("Buzz");
+//                } else {
+//                    System.out.println(i);
+//                }
+//            }
+//        } else {
+//            System.out.println("Please enter a positive number.");
+//        }
+
+
+
+
+        //Q 5  SAME CODE USING WHILE LOOP
+        if(num > 0){
+            int i = 1;
+            while(i <= num) {
+                if(i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if(i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if(i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+                i++;
+            }
+        } else {
+            System.out.println("Please enter a positive number.");
+        }
+    }
+}
