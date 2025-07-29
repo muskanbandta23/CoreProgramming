83fc18fced8450525c4ed77e4d897a06f29f5f46
Muskan Bandta <muskanbandta1@gmail.com>
2025-07-29 10:31:50 +0530

    code

---
+++ arrayslevel1/MeanHeight.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MeanHeight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] heights = new double[11];
+        double sum = 0;
+        for (int i = 0; i < 11; i++) {
+            System.out.print("Player " + (i + 1) + ": ");
+            heights[i] = sc.nextDouble();
+            sum += heights[i];
+        }
+
+        double mean = sum / 11;
+
+        System.out.printf("The mean height of the football team is: %.2f\n", mean);
+
+
+    }
+}
